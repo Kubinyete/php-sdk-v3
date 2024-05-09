75f75677d5ee740d52adb50d117111bd5cff8df4
Vitor Kubinyete <vitorkubinyete@hotmail.com>
2024-05-09 12:29:29 -0300

    Change to Query::build

--- lib/api/PixAccountDetailsApi.php
+++ lib/api/PixAccountDetailsApi.php
@@ -654,7 +654,7 @@ class PixAccountDetailsApi
 
             } else {
                 // for HTTP post (form)
-                $httpBody = \GuzzleHttp\Psr7\build_query($formParams);
+                $httpBody = \GuzzleHttp\Psr7\Query::build($formParams);
             }
         }
 
@@ -675,7 +675,7 @@ class PixAccountDetailsApi
             $headers
         );
 
-        $query = \GuzzleHttp\Psr7\build_query($queryParams);
+        $query = \GuzzleHttp\Psr7\Query::build($queryParams);
         return new Request(
             'GET',
             $this->config->getHost() . $resourcePath . ($query ? "?{$query}" : ''),
